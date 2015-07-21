ef5d22579e38a9fbadd3d6801876f655fd200f14
Andreas Pfeiffer <andreas.pfeiffer@zalando.de>
2015-07-21 16:53:14 +0200

    syntax fix

--- sevenseconds/aws.py
+++ sevenseconds/aws.py
@@ -313,8 +313,8 @@ def configure_dns(account_name, cfg):
         try:
             configure_dns_delegation(account_name, nameservers, cfg)
         except:
-           act.error('DNS Delegation not possible')
-           soa_ttl = cfg.get('domain_soa_ttl', '60')
+            act.error('DNS Delegation not possible')
+    soa_ttl = cfg.get('domain_soa_ttl', '60')
     with Action('Set SOA-TTL to {}..'.format(soa_ttl)):
         rr_list = conn.list_resource_record_sets(HostedZoneId=zone['Id'],
                                                  StartRecordType='SOA',
