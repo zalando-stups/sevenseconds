3887b459f39018caacce765d3480e915df8d2900
Matthias Kerk <matthias@zalando.de>
2017-05-15 16:43:29 +0200

    add support for dynamodb endpoints

--- sevenseconds/config/vpc.py
+++ sevenseconds/config/vpc.py
@@ -502,8 +502,8 @@ def create_vpc_endpoints(account: object, vpc: object, region: str):
     service_names = ec2c.describe_vpc_endpoint_services()['ServiceNames']
 
     for service_name in service_names:
-        if service_name.endswith('.s3'):
-            with ActionOnExit('Checking S3 VPC Endpoints..') as act:
+        if service_name.endswith('.s3') or service_name.endswith('.dynamodb'):
+            with ActionOnExit('Checking VPC Endpoint {}..'.format(service_name)) as act:
                 endpoints = ec2c.describe_vpc_endpoints(
                     Filters=[
                         {
