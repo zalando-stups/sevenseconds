3cce8b4251ebf6e48a70df1914a8513e29949d01
Yerken <yerken.tussupbekov@zalando.de>
2017-06-26 14:05:06 +0200

    support dropping kms-keys (#67)
    * support dropping kms-keys
    * handle the case when key is already deleted
    * put back config key for the aws response

--- sevenseconds/config/kms.py
+++ sevenseconds/config/kms.py
@@ -1,12 +1,17 @@
 from ..helper import ActionOnExit
+from botocore.exceptions import ClientError
 import json
 
 
+# TODO:support reverting Drop:true operation by either cancelling deletion or recreating the keys
 def configure_kms_keys(account: object, region):
     keys_config = account.config.get('kms', {})
     kms_client = account.session.client('kms', region)
     for key_alias in keys_config:
         key_config = keys_config[key_alias]
+        if key_config.get('drop', False):
+            schedule_key_deletion(kms_client, key_alias)
+            continue
         key = json.loads(json.dumps(key_config).replace('{account_id}', account.id))
         with ActionOnExit('Searching for key "{}"..'.format(key_alias)) as act:
             exist_aliases = kms_client.list_aliases()
@@ -18,7 +23,7 @@ def configure_kms_keys(account: object, region):
                     put_key_response = kms_client.put_key_policy(
                         KeyId=alias['TargetKeyId'],
                         PolicyName='default',
-                        Policy=json.dumps(key['KeyPolicy']),
+                        Policy=json.dumps(key['key_policy']),
                         BypassPolicyLockoutSafetyCheck=False
                     )
                     if put_key_response['ResponseMetadata']['HTTPStatusCode'] != 200:
@@ -31,12 +36,12 @@ def configure_kms_keys(account: object, region):
                     break
             if not found:
                 create_response = kms_client.create_key(
-                    Description=key['Description'],
-                    KeyUsage=key['KeyUsage'],
+                    Description=key['description'],
+                    KeyUsage=key['key_usage'],
                     Origin='AWS_KMS',
                     BypassPolicyLockoutSafetyCheck=False,
-                    Policy=json.dumps(key['KeyPolicy']),
-                    Tags=key['Tags']
+                    Policy=json.dumps(key['key_policy']),
+                    Tags=key['tags']
                 )
                 if create_response['ResponseMetadata']['HTTPStatusCode'] != 200:
                     act.error('failed to create a key {} response: {}'.format(key_alias, create_response))
@@ -52,3 +57,31 @@ def configure_kms_keys(account: object, region):
                         .format(key_alias, key_id, alias_response)
                     )
                     continue
+
+
+def schedule_key_deletion(kms_client, key_alias):
+    with ActionOnExit('Checking deletion status for key "{}"..'.format(key_alias)) as act:
+        try:
+            describe_key_response = kms_client.describe_key(
+                KeyId=key_alias
+            )
+        except ClientError as ex:
+            if ex.response['Error']['Code'] == 'NotFoundException':
+                act.ok('key {} cannot be found, probably deleted'.format(key_alias))
+                return
+            else:
+                raise ex
+        if describe_key_response['KeyMetadata']['KeyState'] == 'PendingDeletion':
+            act.ok('key {} is already scheduled for deletion'.format(key_alias))
+            return
+        schedule_response = kms_client.schedule_key_deletion(
+                                KeyId=describe_key_response['KeyMetadata']['KeyId'],
+                                PendingWindowInDays=7,
+                            )
+        if schedule_response['ResponseMetadata']['HTTPStatusCode'] != 200:
+            act.error(
+                'failed to schedule key {} for deletion'
+                .format(key_alias)
+            )
+            return
+        act.ok('successfully scheduled key {} for deletion'.format(key_alias))
