1846777919f32aeb1d9964ab7eb061564366885d
Mikkel Oscar Lyderik Larsen <mikkel.larsen@zalando.de>
2021-06-11 10:13:22 +0200

    Support setting vpc flow log retention time
    Signed-off-by: Mikkel Oscar Lyderik Larsen <mikkel.larsen@zalando.de>

--- sevenseconds/config/cloudwatch.py
+++ sevenseconds/config/cloudwatch.py
@@ -1,7 +1,17 @@
-def configure_log_group(session: object, region: str):
-    client = session.client('logs', region)
-    logs = client.describe_log_groups(logGroupNamePrefix='vpc-flowgroup')['logGroups']
+def configure_log_group(session: object, region: str, config: dict):
+    retention_in_days = config.get("vpc", {}).get("flow_logs_retention_in_days", 3653)
+    client = session.client("logs", region)
+    logs = client.describe_log_groups(logGroupNamePrefix="vpc-flowgroup")["logGroups"]
     for log in logs:
-        if log.get('logGroupName', '') == 'vpc-flowgroup':
+        if log.get("logGroupName", "") == "vpc-flowgroup":
+            if log.get("retentionInDays", 0) != retention_in_days:
+                client.put_retention_policy(
+                    logGroupName="vpc-flowgroup",
+                    retentionInDays=retention_in_days,
+                )
             return
-    client.create_log_group(logGroupName='vpc-flowgroup')
+    client.create_log_group(logGroupName="vpc-flowgroup")
+    client.put_retention_policy(
+        logGroupName="vpc-flowgroup",
+        retentionInDays=retention_in_days,
+    )
